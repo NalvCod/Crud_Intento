0844aeda9286c53b86b8918985ebd5dbc5323095
NalvCod <nahuelvhweb@gmail.com>
2024-12-17 21:00:46 +0100

    aaa

--- app/src/main/java/com/example/firabasecrud/Serie.kt
+++ app/src/main/java/com/example/firabasecrud/Serie.kt
@@ -1,14 +1,24 @@
 package com.example.firabasecrud
 
-import java.io.Serializable
+    import java.io.Serializable
+    import java.text.SimpleDateFormat
+    import java.util.Date
+    import java.util.Locale
 
-data class Serie(
-    var id: String? = "",
-    var nombre: String? = "",
-    var fechaInicio: String? = "",
-    var fechaFin:String?="",
-    var genero:String?="",
-    var url_imagen: String? = "",
-    var id_imagen: String? = "",
-    var puntuacion : Int = 0
-): Serializable
+    data class Serie(
+        var id: String? = "",
+        var nombre: String? = "",
+        var fechaInicio: String? = "",
+        var fechaFin:String?="",
+        var genero:String?="",
+        var url_imagen: String? = "",
+        var id_imagen: String? = "",
+        var puntuacion : Int = 0,
+        var fechaCreacionSerieBD: String = getCurrentDate()
+    ): Serializable
+
+        fun getCurrentDate(): String {
+            val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            return sdf.format(Date())
+        }
+
